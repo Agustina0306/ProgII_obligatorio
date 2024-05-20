82693f6020bdbcb96367e6cdd45f241914826e1b
Ana Menoni <anamenoni@192.168.1.171>
2024-05-20 19:44:34 -0300

    MyBSTImlrev0

---
+++ tad/src/BinarySearchTree/MyBSTIml.java
@@ -0,0 +1,45 @@
+package BinarySearchTree;
+
+import LinkedList.MyList;
+
+public class MyBSTIml <K extends Comparable<K>, T> implements MyBinarySearchTree<K,T>{
+    @Override
+    public void add(K key, T value) {
+
+    }
+
+    @Override
+    public void remove(K key) {
+
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return false;
+    }
+
+    @Override
+    public T find(K key) {
+        return null;
+    }
+
+    @Override
+    public MyList<K> inOrder() {
+        return null;
+    }
+
+    @Override
+    public MyList<K> postOrder() {
+        return null;
+    }
+
+    @Override
+    public MyList<K> preOrder() {
+        return null;
+    }
+
+    @Override
+    public TreeNode<K, T> getRoot() {
+        return null;
+    }
+}
