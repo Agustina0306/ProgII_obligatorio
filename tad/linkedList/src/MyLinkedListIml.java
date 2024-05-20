1c4d7623eabff22dd25913c6143410b790ddebae
Ana Menoni <anamenoni@MacBook-Air-de-Ana-2.local>
2024-05-20 10:57:17 -0300

    MyList

--- tad/linkedList/src/MyLinkedListIml.java
+++ tad/linkedList/src/MyLinkedListIml.java
@@ -1,2 +1,31 @@
-public class MyLinkedListIml {
+public class MyLinkedListIml<T> implements MyList<T> {
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public T get(int position) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(T value) {
+        return false;
+    }
+
+    @Override
+    public void remove(T value) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T get(T value) {
+        return null;
+    }
 }
