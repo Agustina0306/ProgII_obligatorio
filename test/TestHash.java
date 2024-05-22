e9b195202d99a40c54aefe04f7cb7df94bb5e11b
Agustina <Agustina0306@gmail.com>
2024-05-22 16:25:41 -0300

    Test Hash sin terminar

--- test/TestHash.java
+++ test/TestHash.java
@@ -1,6 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
 import tad.LinkedList.MyLinkedListIml;
+import tad.LinkedList.MyList;
 import tad.hash.MyHash;
 import tad.hash.MyHashIml;
 
@@ -15,6 +16,24 @@ public class TestHash {
         hash.insert("one", 1);
         hash.insert("two", 2);
         hash.insert("three", 3);
+
+        assertEquals(3, hash.size());
+
+        MyList<String> keys = hash.keys();
+        assertTrue(keys.contains("one"));
+        assertTrue(keys.contains("three"));
+        assertFalse(keys.contains("two"));
+
+        assertEquals(Integer.valueOf(1), hash.get("one"));
+        assertEquals(Integer.valueOf(2), hash.get("two"));
+        assertEquals(Integer.valueOf(3), hash.get("three"));
+
+
+
+        MyList<Integer> values = hash.values();
+        assertTrue(values.contains(1));
+        assertTrue(values.contains(3));
+        assertFalse(values.contains(2));
 
 
     }
