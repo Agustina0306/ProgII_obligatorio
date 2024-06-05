461e0696c6914d1404bc13758b2863f06e505bff
anamenoni <anamenoni@gmail.com>
2024-06-05 13:22:31 -0300

    Comienzo funcion top10DiaPais

--- src/Spotify.java
+++ src/Spotify.java
@@ -1,7 +1,28 @@
-public class Spotify {
+import entities.Artista;
+import entities.DataLoader;
+import entities.Top50;
+import tad.LinkedList.MyLinkedListIml;
+import tad.LinkedList.MyList;
 
-    public void top10DiaPais (String pais, String fecha){
+public class Spotify<T>  {
+
+    public void top10DiaPais (String pais, String fecha, DataLoader data){
         String keyToFind = pais + fecha;
+        MyList<Top50> top50List = data.getTopEntries().get(keyToFind);
+        for (int i = 10; i < 0; i--){
+            for (int j = 0; j < top50List.size(); j++){
+                if (i == top50List.getPosition(j).getPosicion()){
+                    MyList<Artista> artist = top50List.getPosition(j).getCancion().getArtista();
+                    System.out.println("Nombre Cancion: "  + "Artista: " + "Posicion: " );
+                }
+            }
+        }
 
+    }
+
+    public void printList (MyList<Artista> lista){
+        for (int i = 0; i< lista.size(); i++){
+            System.out.println(lista.getPosition(i));
+        }
     }
 }
