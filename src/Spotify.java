ed795c59c23084c488ae32eec8afb536ad65c22b
Agustina <Agustina0306@gmail.com>
2024-06-14 16:48:44 -0300

    Finalizacion funcion 1

--- src/Spotify.java
+++ src/Spotify.java
@@ -2,30 +2,37 @@ import entities.Artista;
 import entities.Cancion;
 import entities.DataLoader;
 import entities.Top50;
+import exceptions.DatoNoEXiste;
+import tad.LinkedList.DatoInvalido;
 import tad.LinkedList.MyLinkedListIml;
 import tad.LinkedList.MyList;
 
 public class Spotify<T>  {
 
-    public static void top10DiaPais(String pais, String fecha, DataLoader data){
-        String keyToFind = pais + fecha;
-        MyList<Top50> top50List = data.getTopEntries().get(keyToFind);
-        for (int i = 10; i < 0; i--){
-            for (int j = 0; j < top50List.size(); j++){
-                if (i == top50List.getPosition(j).getPosicion()){
-                    MyList<Artista> artistas = top50List.getPosition(j).getCancion().getArtista();
-                    String titulo= top50List.getPosition((j)).getCancion().getTitulo();
-
-                    System.out.println("Nombre Cancion: "+ titulo + "Artista: "+ artistas + "Posicion: "+ j );
-                }
+    public static void top10DiaPais(String pais, String fecha, DataLoader data) throws DatoInvalido, DatoNoEXiste {
+        if (pais==null | fecha==null | data==null){
+            throw new DatoInvalido();
+        }
+        if (!data.getTopEntries().contains(pais + "|"+ fecha + "|" + "1")){
+            throw new DatoNoEXiste();
+        }
+        int i=10;
+        while(i>0){
+            i--;
+            String j= String.valueOf(i);
+            String key = pais + "|"+ fecha + "|" + j;
+            MyList<Artista> artistas = data.getTopEntries().getValue(key).getCancion().getArtista();
+            String titulo= data.getTopEntries().getValue(key).getCancion().getTitulo();
+            String nombresArtistas= null;
+            for (int k = 0; k < artistas.size(); k++) {
+                nombresArtistas +=artistas.getPosition(k);
+                nombresArtistas+= " | ";
             }
+
+            System.out.println("Nombre Cancion: "+ titulo + "Artista: "+ nombresArtistas + "Posicion: "+ j );
         }
 
     }
 
-    public void printList (MyList<Artista> lista){
-        for (int i = 0; i< lista.size(); i++){
-            System.out.println(lista.getPosition(i));
-        }
-    }
+
 }
---
+++ src/exceptions/DatoNoEXiste.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class DatoNoEXiste extends Exception{
+}
