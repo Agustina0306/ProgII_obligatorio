8755ddd6308fe35068cd2ff9172dd011c35850b6
anamenoni <anamenoni@gmail.com>
2024-06-14 20:12:48 -0300

    funcion3

--- src/Spotify.java
+++ src/Spotify.java
@@ -7,6 +7,7 @@ import tad.LinkedList.DatoInvalido;
 import tad.LinkedList.MyLinkedListIml;
 import tad.LinkedList.MyList;
 import tad.heap.MyHeap;
+import tad.heap.MyHeapImpl;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -46,11 +47,45 @@ public class Spotify{
         if (fecha == null){
             throw new DatoInvalido();
         }
-
         LocalDate date = LocalDate.parse(fecha);
         MyHeap<Top50> cancionesFecha = data.getTop50Fecha().getValue(date);
         for (int i = 0; i < 5 ; i++) {
             System.out.println(cancionesFecha.delete().getCancion().getTitulo());
+        }
+
+    }
+
+    public static void Top7ArtistasEnRango (String fechaInicio, String fechaFin, DataLoader data){
+        LocalDate inicio = LocalDate.parse(fechaInicio);
+        LocalDate fin = LocalDate.parse(fechaFin);
+        MyList<LocalDate> rangeDate = new MyLinkedListIml<>();
+        LocalDate currentDate = inicio;
+
+        MyHeap<Artista> artistasExitosos = new MyHeapImpl<>(false);
+
+        while (!currentDate.isAfter(fin)){
+            rangeDate.add(currentDate);
+            currentDate.plusDays(1);
+        }
+
+        for (int i = 0; i < rangeDate.size(); i++) {
+            MyHeap<Top50> cancionesFecha = data.getTop50Fecha().getValue(rangeDate.getPosition(i));
+            Cancion tempCancion = cancionesFecha.delete().getCancion();
+            MyList<Artista> artistas = tempCancion.getArtista();
+
+            for (int j = 0; j < artistas.size(); j++) {
+                Artista tempArtista = artistas.getPosition(j);
+                if (artistasExitosos.search(tempArtista) != null && artistasExitosos.search(tempArtista).equals(tempArtista)) {
+                    artistasExitosos.search(tempArtista).setArtistaCounter();
+                } else {
+                    tempArtista.setArtistaCounter();
+                    artistasExitosos.insert(tempArtista);
+                }
+            }
+        }
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(artistasExitosos.delete().getNombre());
         }
 
     }
--- src/entities/Artista.java
+++ src/entities/Artista.java
@@ -3,14 +3,17 @@ package entities;
 import tad.LinkedList.MyLinkedListIml;
 import tad.LinkedList.MyList;
 
-public class Artista {
+public class Artista implements Comparable<Artista> {
 
     private String nombre;
     private MyList<Cancion> cancionesArtista;
 
+    private int artistaCounter;
+
     public Artista(String nombre) {
         this.nombre = nombre;
         cancionesArtista = new MyLinkedListIml<>();
+        this.artistaCounter = 0;
     }
 
     public String getNombre() {
@@ -20,4 +23,18 @@ public class Artista {
         return cancionesArtista;
     }
 
+    public void setArtistaCounter() {
+        this.artistaCounter++;
+    }
+
+    @Override
+    public int compareTo(Artista o) {
+        if (this.artistaCounter > o.artistaCounter){
+            return 1;
+        } else if (this.artistaCounter < o.artistaCounter) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
